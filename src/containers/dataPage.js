5fa5a2342104ca3052da29c62fd88805f80baf7a
Subeg Aryal <subeg7@gmail.com>
2020-04-08 21:56:22 +0545

    dev : useRef hook

--- src/containers/dataPage.js
+++ src/containers/dataPage.js
@@ -1,20 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { Component, useRef, useEffect } from 'react';
+import * as d3 from 'd3';
 
-const Datapage = props => {
-
-  useEffect(() => {
-    
-  });
-
-  return (
-    <div>
-      <h1> Data Page</h1>
-    </div>
-  );
-}
-
-export default connect(
-  null,
-  null
-)(Datapage);
+const DatapageHooks = props => {
+    useEffect(() => {
+        tempRef.current.style = 'background-color: green;'
+    })
+    const temperatureData = [8, 5, 13, 9, 12];
+    const tempRef = useRef(null);
+    d3.select(tempRef)
+            .selectAll("h1")
+            .data(temperatureData)
+            .enter()
+            .append("h2")
+            .text("New Temperature");
+    return (<div ref={tempRef}><h1>DatapageHooks</h1></div>);
+  }
+export default DatapageHooks;
