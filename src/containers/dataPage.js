ef16ef1446bb2d0eb0585fd778f10b23c046a9b8
Subeg Aryal <subeg7@gmail.com>
2020-04-09 21:36:57 +0545

    dev : first use of d3 for styling

--- src/containers/dataPage.js
+++ src/containers/dataPage.js
@@ -2,17 +2,19 @@ import React, { Component, useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 
 const DatapageHooks = props => {
-    useEffect(() => {
-        tempRef.current.style = 'background-color: green;'
-    })
-    const temperatureData = [8, 5, 13, 9, 12];
-    const tempRef = useRef(null);
-    d3.select(tempRef)
-            .selectAll("h1")
-            .data(temperatureData)
-            .enter()
-            .append("h2")
-            .text("New Temperature");
-    return (<div ref={tempRef}><h1>DatapageHooks</h1></div>);
-  }
+  useEffect(() => {
+    // tempRef.current.style = 'background-color: green;';
+    d3.select(tempRef.current)
+      .selectAll("h2")
+      .data(temperatureData)
+      .enter()
+      .append("h2")
+      .text((datapoint) => datapoint + " degrees")
+      .style("background-color", "red");
+  })
+  const temperatureData = [8, 5, 13, 9, 12];
+  const tempRef = useRef(null);
+
+  return (<div ref={tempRef}><h1>DatapageHooks</h1></div>);
+}
 export default DatapageHooks;
