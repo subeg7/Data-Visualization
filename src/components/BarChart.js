081553d2b6655381ee79585b5fa4cc93863159f1
Subeg Aryal <subeg7@gmail.com>
2020-04-09 22:17:22 +0545

    dev : data visualized in bar chart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -6,7 +6,7 @@ const BarChart = props => {
     const canvasRef = useRef(null);
 
     useEffect(() => {
-        const data = [2, 4, 2, 6, 8];
+        const data = [3, 5, 1, 7, 2];
         drawBarChart(data);
     });
 
@@ -14,11 +14,32 @@ const BarChart = props => {
         // canvasRef.current.style = 'background-color: yellow;';
         console.log("drawing bar chart");
 
+        const canvasHeight = 400;
+        const canvasWidth = 600;
+        const scale = 20;
+
         const svgCanvas = d3.select(canvasRef.current)
             .append("svg")
             .attr("width", 600)
             .attr("height", 400)
-            .style("border", "12px solid black")
+            .style("border", "5px solid black");
+
+        svgCanvas.selectAll("rect")
+            .data(data).enter()
+            .append("rect")
+            .attr("width", 40)
+            .attr("height", (datapoint) => datapoint * 20)
+            .attr("fill", "orange")
+            .attr("x", (datapoint, iteration) => iteration * 45)
+            .attr("y", (datapoint) => canvasHeight - datapoint * scale)
+            .text(dataPoint => dataPoint);
+
+        svgCanvas.selectAll("text")
+            .data(data).enter()
+            .append("text")
+            .attr("x", (dataPoint, i) => i * 45 + 10)
+            .attr("y", (dataPoint, i) => canvasHeight - dataPoint * scale - 10)
+            .text(dataPoint => dataPoint)
     };
 
 
